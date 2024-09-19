2b2f67a0ab350de4e74ebc3531985e58a39d03b4
Alejandro Kaleb Leyte Garcia <78818308+kal-leyte@users.noreply.github.com>
2024-09-19 09:19:08 -0600

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,14 +1,17 @@
-#Código para enviar mensajes a whatsapp
+import pyautogui as pgLibreria
+import webbrowser as ventanaWeb
+import time as tiempo
 
-#importación de librerias
-import pyautogui as pgLibreria, webbrowser as ventanaWeb, time as tiempo
-#Donde se abrira el programa y a quien enviara el mensaje
-ventanaWeb.open("https://web.whatsapp.com/send?phone=+525523703521")
-#Tiempo de espera del programa para enviar los mensajes
-tiempo.sleep(5)
+# Abre WhatsApp Web con el número de teléfono especificado
+ventanaWeb.open("https://web.whatsapp.com/send?phone=+525637198658")
 
-#For de cuantas veces se embiara el código
-for x in range(15):
-    pgLibreria.write("El dominio total xd")#mensaje enviado
-    pgLibreria.press('enter')#Hace alusión al boton de enviar
-    pass
+# Tiempo de espera para que WhatsApp Web cargue completamente
+tiempo.sleep(20)  # Aumenté el tiempo de espera para asegurar que la página cargue
+
+# Enviar el mensaje automáticamente 30 veces
+for x in range(30):
+    # Espera un poco antes de escribir el mensaje para asegurarse de que el chat esté listo
+    tiempo.sleep(2)
+    pgLibreria.write("¿Ya tienes tu respuesta? >:v")  # Mensaje enviado
+    pgLibreria.press('enter')  # Hace alusión al botón de enviar
+
